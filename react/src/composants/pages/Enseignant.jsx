c9e6f1bec640c162152db75d695a0b4a0109a2fa
Pierre-Olivier Clerson <poclerson@gmail.com>
2022-10-23 15:43:02 -0400

    Gestion de l'ouverture de chaque enseignant

--- react/src/composants/pages/Enseignant.jsx
+++ react/src/composants/pages/Enseignant.jsx
@@ -1,18 +1,24 @@
 import './Enseignant.scss';
-
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import {useState} from 'react';
 
-export default function Enseignant({nom, description, photo, domaine}) {
-    const [ouverture, setOuverture] = useState("ferme");
+export default function Enseignant({nom, description, photo, domaine, gestionClicListe}) {
+    const [ouverture, setOuverture] = useState('ferme');
+
+    const gestionClic = () => {
+        gestionClicListe();
+        setOuverture(ouverture == 'ouvert' ? 'ferme' : 'ouvert');
+    }
 
     return(
-        <li className="Enseignant">
-            <div className="miniature" onClick={() => setOuverture(ouverture == "ferme" ? "ouvert" : "ferme")}>
+        <li className={"Enseignant " + ouverture}>
+            <div className="miniature" onClick={gestionClic}>
                 <h3 className="titre">{nom}</h3>
                 <img className="photo" src={photo} alt={"Photo de " + nom} />
                 <p className="domaine">{domaine}</p>
             </div>
             <div className={"contenu " + ouverture}>
+                <ArrowBackIosIcon className="Icone" onClick={gestionClic} />
                 <h3 className="titre">{nom}</h3>
                 <p className="desc">{description}</p>
                 <p className="domaine">{domaine}</p>
--- react/src/composants/pages/ListeEnseignants.jsx
+++ react/src/composants/pages/ListeEnseignants.jsx
@@ -1,23 +1,30 @@
+import './ListeEnseignants.scss';
+
 import useObtenir from '../../hooks/useObtenir';
+import {useState} from 'react';
 
 import Enseignant from './Enseignant';
 import Chargement from '../modules/Chargement';
 
-import './ListeEnseignants.scss';
-
 export default function ListeEnseignants() {
     const enseignants = useObtenir('/enseignants', 'bre');
+    const [listeOuverte, setListeOuverte] = useState('ouvert');
+
+    const gestionClicListe = () => {
+        setListeOuverte(listeOuverte == 'ouvert' ? 'ferme' : 'ouvert');
+    }
 
     return(
         enseignants != null ?
             <section className="ListeEnseignants">    
                 <h1 className="titre">nos enseignants.</h1>
 
-                <ul className="liste">
+                <ul className={"liste " + listeOuverte}>
                     {enseignants.map(enseignant => 
                         <Enseignant 
                             {... enseignant.acf}
                             key={enseignant.id} 
+                            gestionClicListe={gestionClicListe}
                         />
                     )}
                 </ul>
