6931891e86922b9095883fedb9da6f232221c45f
Baaaaguette <lilikikou2@gmail.com>
2022-11-02 09:15:10 -0400

    Footer font correction

--- react/src/composants/navigation/PiedPage.jsx
+++ react/src/composants/navigation/PiedPage.jsx
@@ -6,9 +6,6 @@ export default function PiedPage() {
             <svg className="vague" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
             <div className="contenu">
                 {/* Ajouter les éléments du footer ici */}
-<<<<<<< Updated upstream
-                <h2 className="titre">Ceci est le footer</h2>
-=======
 
                 <div className="logo">
                 <SiteLogo url={enteteWP.siteLogoUrl} />
@@ -31,7 +28,6 @@ export default function PiedPage() {
                  <h3 className="sous-titre">Réseaux sociaux</h3>   
                 </div>
 
->>>>>>> Stashed changes
             </div>
 
         </footer>
