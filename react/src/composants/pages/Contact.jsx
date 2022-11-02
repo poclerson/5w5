c40cbf63dd79073cc944c2085755846d5d4e3046
Ngoc Quynh Pham <nquynhp2002@yahoo.ca>
2022-11-01 22:47:41 -0400

    Ajustement du texte de description et positionnement de 'Inscription'

--- react/src/composants/pages/Contact.jsx
+++ react/src/composants/pages/Contact.jsx
@@ -20,22 +20,15 @@ export default function Contact() {
                 </div>
                 <div className='informations'>
                     <h3>Description</h3>
-                    <p>Au cours de ta formation, tu apprendras à identifier, analyser, imaginer, réaliser et coordonner différents projets numériques et les différentes étapes de production qui y sont liées :
-                        La médiatisation – Créer ou modifier des éléments graphiques et multimédias.
-                        La conception –Concevoir le design et l’interactivité d’un projet multimédia.
-                        L’intégration – Intégrer de façon cohérente et artistique les composantes d’un projet interactif.
-                        L’environnement professionnel – Développer une vision globale du multimédia et connaître l’environnement de travail tant du point de vue technologique qu’interpersonnel.
-                        Les logiciels – Photoshop, Illustrator, Maya (3D), Unity, After Effects, etc.
-                        Les langages – HTML, CSS, JavaScript, SQL, PHP, C#, etc.
-                        56 % de ta formation spécifique se déroulera en laboratoire et en stage
-                        Tu réaliseras des projets d’envergure diversifiés, amusants et stimulants qui te permettront d’explorer les multiples facettes de l’intégration multimédia.
-                        Sites Web, jeux interactifs, applications mobiles, animations 2D/3D, créations audio et vidéo.
-                        Un stage crédité de 8 semaines en entreprise.</p>
+                    <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non odio euismod lacinia at quis risus sed. Eu nisl nunc mi ipsum faucibus. Eget mauris pharetra et ultrices neque ornare aenean euismod. 
+
+                    </p>
                 </div>
             </article>
 
             <div className='inscription'>
-                <h2>Inscription</h2>
+                <h2 className='titre-inscription'>Inscription</h2>
                 <ArrowForwardIosIcon className='Icone'/>
             </div>
 
