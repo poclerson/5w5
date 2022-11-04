51d12dfb8b65f528d872051be2a18fea6bada0ab
Pierre-Olivier Clerson <poclerson@gmail.com>
2022-11-04 00:19:09 -0400

    Optimisation de la page enseignants (meilleure gestion des ouvertures)

--- react/src/composants/pages/ListeEnseignants.jsx
+++ react/src/composants/pages/ListeEnseignants.jsx
@@ -1,8 +1,7 @@
 import './ListeEnseignants.scss';
 
 import useObtenir from '../../hooks/useObtenir';
-import {useState, useEffect} from 'react';
-import * as boites from '../../boites';
+import {useState} from 'react';
 
 import Enseignant from './Enseignant';
 import Chargement from '../modules/Chargement';
@@ -10,180 +9,37 @@ import Suivant from '../modules/Suivant';
 
 export default function ListeEnseignants({titre}) {
     const enseignants = useObtenir('/enseignants', 'bre');
-    const [listeOuverte, setListeOuverte] = useState('ouvert');
-    const [ouvertures, setOuvertures] = useState(null);
-
-    // Quand on ouvre/ferme la liste
-    const gestionClicListe = (index, ouverture) => {
-        // S'il était fermé, on ouvre
-        if (ouverture == 'ferme')
-            setOuvertures(boites.ouvrir(index, enseignants.map(() => 'ferme')))
-
-        // Sinon on ferme
-        else 
-            setOuvertures(enseignants.map(() => 'ferme'))
-    }
+    const [indexOuvert, setIndexOuvert] = useState(-1);
 
     // Quand on clique sur le bouton suivant (ordinateur seulement)
     const gestionClicSuivant = () => {
-        setOuvertures(
-            boites.ouvrir(
-                boites.obtenirOuverte(ouvertures) + 1,
-                enseignants.map(() => 'ferme')
-            )
-        )
+        if (indexOuvert + 1 == enseignants.length) {
+            setIndexOuvert(0);
+            return;
+        }
+
+        setIndexOuvert(indexOuvert + 1);
     }
-
-    // Initialiser les états d'ouverture
-    useEffect(() => {
-        if (enseignants != null) {
-            setOuvertures(enseignants.map(() => 'ferme'))
-        }
-    }, [enseignants])
-
-    useEffect(() => {
-        if (ouvertures) {
-            // S'il y a une boite ouverte, on doit mettre l'état d'ouverture de la liste à fermé
-            ouvertures.includes('ouvert') ?
-
-            setListeOuverte('ferme') : setListeOuverte('ouvert');
-        }
-    }, [ouvertures])
 
     return(
         enseignants != null ?
-            <section className="ListeEnseignants">
-                <h1 className={"titre " + listeOuverte}>nos <br/> enseignants.</h1>
-
-                <ul className={"liste " + listeOuverte}>
+            <section className="ListeEnseignants" enseignant-ouvert={indexOuvert != -1 ? "true" : "false"} >
+                <h1 className="titre">nos <br/> enseignants.</h1>
+                <ul className="liste">
                     {
-                        // Vérifier l'existence d'ouvertures
-                        ouvertures != null ?
-
-                            // Vérifier qu'au moins une boite est ouverte
-                            ouvertures.indexOf('ouvert') != -1 ? 
-
-                                // Si c'est le cas, afficher toutes les boites après celle qui l'est
-                                enseignants.map((enseignant, index) => {
-                                    if (index > ouvertures.indexOf('ouvert') - 1) {
-                                        return <Enseignant 
-                                            {... enseignant.acf}
-                                            key={enseignant.id} 
-                                            gestionClicListe={gestionClicListe}
-                                            index={index}
-                                            ouverture={ouvertures[index]}
-                                        />
-                                    }
-                                }) 
-
-                                // Sinon 
-                                : enseignants.map((enseignant, index) => 
-                                    <Enseignant 
-                                        {... enseignant.acf}
-                                        key={enseignant.id} 
-                                        gestionClicListe={gestionClicListe}
-                                        index={index}
-                                        ouverture={ouvertures[index]}
-                                    />
-                                ) : <Chargement />
+                        enseignants.map((enseignant, index) => 
+                            <Enseignant 
+                                ouvert={index == indexOuvert ? "true" : "false"}
+                                {... enseignant.acf}
+                                key={enseignant.id} 
+                                index={index}
+                                setIndexOuvert={setIndexOuvert}
+                            />
+                        ) 
                     }
                 </ul>
                 <Suivant gestionClic={gestionClicSuivant} />
-            </section>  : <Chargement />
+            </section>  
+        : <Chargement />
     );
 }
-
-// import './ListeEnseignants.scss';
-
-// import useObtenir from '../../hooks/useObtenir';
-// import { useState, useEffect } from 'react';
-// import * as boites from '../../boites';
-// import useOuvertures from '../../hooks/useOuvertures';
-
-// import Enseignant from './Enseignant';
-// import Chargement from '../modules/Chargement';
-// import Suivant from '../modules/Suivant';
-
-// export default function ListeEnseignants() {
-//     const enseignants = useObtenir('/enseignants', 'bre');
-//     // const [listeOuverte, setListeOuverte] = useState('ouvert');
-//     // const [ouvertures, setOuvertures] = useState(null);
-
-//     const {
-//         ouvertures: ouvertures,
-//         setOuvertures: setOuvertures,
-//         ouvertureListe: ouvertureListe,
-//         setOuvertureListe: setOuvertureListe
-//     } = useOuvertures(enseignants, false);
-
-//     const gestionClicListe = (index, ouverture) => {
-//         console.log(index,ouverture)
-//         // S'il était fermé, on ouvre
-//         if (ouverture == 'ferme')
-//             setOuvertures(boites.ouvrir(index, enseignants.map(() => 'ferme')))
-
-//         // Sinon on ferme
-//         else
-//             setOuvertures(enseignants.map(() => 'ferme'))
-//     }
-
-//     const gestionClicSuivant = () => {
-//         setOuvertures(
-//             boites.ouvrir(
-//                 boites.obtenirOuverte(ouvertures) + 1,
-//                 enseignants.map(() => 'ferme')
-//             )
-//         )
-//     }
-
-//     const truc = () => {
-//         return <>
-//             <h1 className={"titre " + ouvertureListe}>nos enseignants.</h1>
-//             <ul className={"liste " + ouvertureListe}>
-//             {console.log(ouvertures)}
-//             {/* // Vérifier qu'au moins une boite est ouverte */}
-//             {
-//                 ouvertures.indexOf('ouvert') != -1 ?
-
-//                     // Si c'est le cas, afficher toutes les boites après celle qui l'est
-//                     enseignants.map((enseignant, index) => {
-//                         if (index > ouvertures.indexOf('ouvert') - 1) {
-//                             return <Enseignant
-//                                 {...enseignant.acf}
-//                                 key={enseignant.id}
-//                                 gestionClicListe={gestionClicListe}
-//                                 index={index}
-//                                 ouverture={ouvertures[index]}
-//                             />
-//                         }
-//                     })
-
-//                     // Sinon 
-//                     : enseignants.map((enseignant, index) =>
-//                         <Enseignant
-//                             {...enseignant.acf}
-//                             key={enseignant.id}
-//                             gestionClicListe={gestionClicListe}
-//                             index={index}
-//                             ouverture={ouvertures[index]}
-//                         />
-//                     )
-//                 }
-//                 <Suivant gestionClic={gestionClicSuivant} />
-//             </ul>
-//         </>
-//     }
-
-//     return(
-//         enseignants != null ?
-//             <section className="ListeEnseignants">
-//                 {
-//                     // Vérifier l'existence d'ouvertures
-//                         ouvertures != null ? 
-//                             truc()
-//                         : <Chargement />
-//                 }
-//         </section>   
-//         : <Chargement />
-//     );
-// }
