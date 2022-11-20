779a9c5d116de7ba00365c60fa1c8f6815280920
Baaaaguette <lilikikou2@gmail.com>
2022-11-20 18:03:56 -0500

    update

--- react/src/composants/pages/Contact.jsx
+++ react/src/composants/pages/Contact.jsx
@@ -15,7 +15,8 @@ export default function Contact({id}) {
         description,
         titreInscription,
         wpBlockImageSizeMedium,
-        wpBlockWebfactoryMap
+        wpBlockWebfactoryMap,
+        wpBlockSocialLinks
     } = useStructure(id);
     return (
         degrades != null ?
