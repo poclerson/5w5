368bcb4fd45e6fc5317120525f13be13c2e73e79
Ngoc Quynh Pham <nquynhp2002@yahoo.ca>
2022-11-22 17:20:11 -0500

    Positionnement de l'icône 3D

--- react/src/composants/pages/Cours.jsx
+++ react/src/composants/pages/Cours.jsx
@@ -15,9 +15,9 @@ export default function Cours({titre, description, modele, id, index, ouvert}) {
             rotation: [-.1, 5, -.05]
         },
         
-        'https://timm175.sg-host.com/wp-content/uploads/2022/11/videoCamera.glb': {
-            position: [2, 1, 1],
-            rotation: [9, 2, 1]
+        'https://timm175.sg-host.com/wp-content/uploads/2022/11/camera-1.glb': {
+            position: [8, -5, 6],
+            rotation: [0, 2, 0.4]
         },
 
         'https://timm175.sg-host.com/wp-content/uploads/2022/11/web1.glb': {
@@ -26,8 +26,8 @@ export default function Cours({titre, description, modele, id, index, ouvert}) {
         },
 
         'https://timm175.sg-host.com/wp-content/uploads/2022/11/3d-1.glb': {
-            position: [0, 3, 1],
-            rotation: [0, 1, 2]
+            position: [2.5, -.3, 1.9],
+            rotation: [0, 4.3, 0]
         },
 
         'https://timm175.sg-host.com/wp-content/uploads/2022/11/designCours.glb': {
