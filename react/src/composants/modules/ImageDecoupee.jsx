92de4dc451df5bab5dc92a522576c96028d49bcf
Pierre-Olivier Clerson <poclerson@gmail.com>
2022-12-01 18:32:39 -0500

    Enseignant qui dépasse sur tablette

---
+++ react/src/composants/modules/ImageDecoupee.jsx
@@ -0,0 +1,28 @@
+// import './ImageDecoupee.scss';
+// import 'react-image-crop/src/ReactCrop.scss';
+
+// import ReactCrop from 'react-image-crop';
+
+// import {useRef, useEffect, useState} from 'react';
+
+// export default function ImageDecoupee({source, limites}) {
+//     const [contexte, setContexte] = useState(null);
+//     const refCanvas = useRef();
+
+//     return (
+//         <ReactCrop 
+//             crop={{
+//                 unit: '%',
+//                 x: 50,
+//                 y: 50,
+//                 width: 50,
+//                 height: 50
+//             }}
+//             style={{
+//                 cursor: 'unset'
+//             }}
+//         >
+//             <img src={source}/>
+//         </ReactCrop>
+//     )
+// }
--- react/src/composants/pages/Enseignant.jsx
+++ react/src/composants/pages/Enseignant.jsx
@@ -1,13 +1,25 @@
 import './Enseignant.scss';
 import FlecheNav from '../modules/FlecheNav';
+// import ImageDecoupee from '../modules/ImageDecoupee';
 
+import {useRef, useState, useEffect} from 'react';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import medias from '../../medias';
 
 export default function Enseignant({nom, description, photo, domaine, surClic, index, verifierOuverture, id}) {
-    const ordinateur = useMediaQuery(medias.ordinateur);
+    const tablette = useMediaQuery(medias.tablette);
+    const tabletteMax = useMediaQuery(medias.ordinateur, 'max');
+    const refFondPhoto = useRef(null);
+
+    useEffect(() => {
+        if (refFondPhoto.current)
+        setFondPhoto(refFondPhoto.current)
+    }, [refFondPhoto])
+
+    const [fondPhoto, setFondPhoto] = useState(null);
+
     return(
-        <li className={`Enseignant ${domaine}`} id={id} index={index} ouvert={verifierOuverture(index)}>
+        <li onClick={() => console.log(refFondPhoto)} className={`Enseignant ${domaine}`} id={id} index={index} ouvert={verifierOuverture(index)}>
 
             {/* Prévisualisation (image du prof) */}
             <div className="miniature" onClick={() => surClic(index)}>
@@ -26,16 +38,23 @@ export default function Enseignant({nom, description, photo, domaine, surClic, i
                     direction="precedent"
                     classesAdditionnelles="retour"
                 />
-                <div className={"conteneur-photo " + domaine}>
-                    <h2 className="titre">{nom.toUpperCase()}</h2>
-                    <div className="fond-photo">
-                        <img className="photo" src={typeof photo != 'boolean' ? photo : undefined} alt={"Photo de " + nom} />
+                    <div className={"conteneur-photo " + domaine}>
+                        <h2 className="titre">{nom.toUpperCase()}</h2>
+                        <div className="fond-photo">
+                            {/* {tablette && tabletteMax && refFondPhoto.current &&
+                                    <ImageDecoupee source={photo} limites={{
+                                        x: refFondPhoto.current.clientWidth,
+                                        y: -1
+                                    }} /> 
+                            } */}
+                            <div className="agrandisseur">
+                                <img className="photo" ref={refFondPhoto} src={typeof photo != 'boolean' ? photo : undefined} alt={"Photo de " + nom} />
+                            </div>
+                        </div>
+                        <span className="conteneur-domaine">
+                            <p className="domaine">{domaine.toUpperCase()}</p>
+                        </span>
                     </div>
-                    <span className="conteneur-domaine">
-                        <p className="domaine">{domaine.toUpperCase()}</p>
-                    </span>
-                </div>
-
                 {/* Boite de texte */}
                 <article className="texte">
                     <h5 className="etiquette-titre">nom</h5>
